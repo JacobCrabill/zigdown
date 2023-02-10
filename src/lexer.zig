b7e4e4c4566fa4b52148e5349f3d80edd01604f9
JacobCrabill <jacob.crabill@gmail.com>
2023-02-10 08:56:44 -0800

    lexer: Now with better functionality!

--- src/lexer.zig
+++ src/lexer.zig
@@ -1,14 +1,21 @@
 const std = @import("std");
+
 const Allocator = std.mem.Allocator;
 const ArrayList = std.ArrayList;
+const GPA = std.heap.GeneralPurposeAllocator;
 
 pub const TokenType = enum {
     END,
     INVALID,
-    TEXT,
+    WORD,
     BREAK,
     HEADER,
-    CODE,
+    HASH1,
+    HASH2,
+    HASH3,
+    HASH4,
+    CODE_BLOCK,
+    CODE_INLINE,
     QUOTE,
     STAR,
     USCORE,
@@ -23,7 +30,7 @@ pub const Lexer = struct {
     data: []const u8 = undefined,
     cursor: usize = 0,
 
-    // Create a new Lexer from the text of a document
+    /// Create a new Lexer from the text of a document
     pub fn init(text: []const u8) Lexer {
         return Lexer{
             .data = text,
@@ -31,12 +38,12 @@ pub const Lexer = struct {
         };
     }
 
-    // Increment the cursor until we reach a non-whitespace character
+    /// Increment the cursor until we reach a non-whitespace character
     pub fn trimLeft(self: *Lexer) void {
-        while (self.cursor < self.data.len and is_whitespace(self.data[self.cursor])) : (self.cursor += 1) {}
+        while (self.cursor < self.data.len and isWhitespace(self.data[self.cursor])) : (self.cursor += 1) {}
     }
 
-    // Consume the remainder of the current line and return if a newline was found
+    /// Consume the remainder of the current line and return if a newline was found
     pub fn eatLine(self: *Lexer) bool {
         const end_opt: ?usize = std.mem.indexOfScalarPos(u8, self.data, self.cursor, '\n');
         if (end_opt) |end| {
@@ -48,7 +55,7 @@ pub const Lexer = struct {
         }
     }
 
-    // Try parsing a header line
+    /// Try parsing a header line
     pub fn parseHeader(self: *Lexer) ?Token {
         if (self.data[self.cursor] == '#') {
             // Header
@@ -65,19 +72,49 @@ pub const Lexer = struct {
         return null;
     }
 
-    // Try parsing a paragraph break (blank line)
-    pub fn parseBreak(self: *Lexer) ?Token {
-        if (self.data[self.cursor] == '\n') {
-            var token = Token{};
-            token.kind = TokenType.BREAK;
-            token.text = "";
+    /// Try parsing one or more hash characters
+    pub fn parseHash(self: *Lexer) ?Token {
+        const text = self.data[self.cursor..];
+        var token = Token{};
+        if (std.mem.startsWith(u8, text, "####")) {
+            token.kind = TokenType.HASH4;
+            token.text = self.data[self.cursor .. self.cursor + 4];
+            self.cursor += 4;
+            return token;
+        } else if (std.mem.startsWith(u8, text, "###")) {
+            token.kind = TokenType.HASH3;
+            token.text = self.data[self.cursor .. self.cursor + 3];
+            self.cursor += 3;
+            return token;
+        } else if (std.mem.startsWith(u8, text, "##")) {
+            token.kind = TokenType.HASH2;
+            token.text = self.data[self.cursor .. self.cursor + 2];
+            self.cursor += 2;
+            return token;
+        } else if (std.mem.startsWith(u8, text, "#")) {
+            token.kind = TokenType.HASH1;
+            token.text = self.data[self.cursor .. self.cursor + 1];
             self.cursor += 1;
             return token;
+        }
+
+        return null;
+    }
+
+    /// Try parsing a line break
+    pub fn parseBreak(self: *Lexer) ?Token {
+        // TODO: Handle \r\n
+        if (self.data[self.cursor] == '\n') {
+            self.cursor += 1;
+            return Token{
+                .kind = TokenType.BREAK,
+                .text = "",
+            };
         }
         return null;
     }
 
-    // Try parsing a quote line
+    /// Try parsing a quote line
     pub fn parseQuote(self: *Lexer) ?Token {
         if (self.data[self.cursor] == '>') {
             var token = Token{};
@@ -93,49 +130,54 @@ pub const Lexer = struct {
         return null;
     }
 
-    // Try parsing a code block
+    /// Try parsing a code tag (block or inline)
     pub fn parseCode(self: *Lexer) ?Token {
-        if (self.cursor + 3 < self.data.len) {
-            // Check for code block begin tag
-            const tag = "```";
-            const start: usize = self.cursor + 3;
-            const beg = self.data[self.cursor .. self.cursor + 3];
-            if (std.mem.eql(u8, beg, tag)) {
-                var token = Token{};
-                token.kind = TokenType.CODE;
-
-                var end: usize = self.data.len;
-                if (std.mem.indexOf(u8, self.data[start..], tag)) |idx| {
-                    end = start + idx + 3;
-                    token.text = self.data[start .. end - 3];
-                } else {
-                    token.text = self.data[start..];
-                }
-
-                self.cursor = end + 1;
-                return token;
-            }
-        }
-        return null;
-    }
-
-    // Try parsing a line of generic text
-    pub fn parseText(self: *Lexer) ?Token {
-        if (std.ascii.isASCII(self.data[self.cursor])) {
-            var token = Token{};
-            // Generic text
-            token.kind = TokenType.TEXT;
-            const cursor = self.cursor;
-            if (self.eatLine()) {
-                token.text = self.data[cursor .. self.cursor - 1];
-            } else {
-                token.text = self.data[cursor..self.cursor];
-            }
+        const text = self.data[self.cursor..];
+        if (std.mem.startsWith(u8, text, "```")) {
+            const token = Token{
+                .kind = TokenType.CODE_BLOCK,
+                .text = self.data[self.cursor .. self.cursor + 3],
+            };
+            self.cursor += 3;
             return token;
         }
+
+        if (std.mem.startsWith(u8, text, "`")) {
+            const token = Token{
+                .kind = TokenType.CODE_INLINE,
+                .text = self.data[self.cursor .. self.cursor + 1],
+            };
+            self.cursor += 1;
+            return token;
+        }
+
         return null;
     }
 
+    /// Try parsing a line of generic text
+    pub fn parseText(self: *Lexer) ?Token {
+        const text = self.data[self.cursor..];
+        var end = self.data.len;
+        for (text) |c, i| {
+            if (!std.ascii.isASCII(c) or isWhitespace(c) or isLineBreak(c)) {
+                end = self.cursor + i;
+                break;
+            }
+        }
+
+        if (end > self.cursor) {
+            const start = self.cursor;
+            self.cursor = end;
+            return Token{
+                .kind = TokenType.WORD,
+                .text = self.data[start..end],
+            };
+        }
+
+        return null;
+    }
+
+    /// Consume the next token in the text
     pub fn next(self: *Lexer) Token {
         var token = Token{};
 
@@ -143,8 +185,8 @@ pub const Lexer = struct {
 
         if (self.cursor >= self.data.len) return token;
 
-        if (self.parseHeader()) |header| {
-            return header;
+        if (self.parseHash()) |hash| {
+            return hash;
         }
 
         if (self.parseBreak()) |br| {
@@ -168,17 +210,30 @@ pub const Lexer = struct {
     }
 };
 
-pub fn is_whitespace(c: u8) bool {
-    if (c == ' ' or c == '\t')
+/// Check if the character is a whitespace character
+pub fn isWhitespace(c: u8) bool {
+    const ws_chars = " \t\r";
+    if (std.mem.indexOfScalar(u8, ws_chars, c)) |_| {
         return true;
+    }
+
+    return false;
+}
+
+/// Check if the character is a line-break character
+pub fn isLineBreak(c: u8) bool {
+    const ws_chars = "\r\n";
+    if (std.mem.indexOfScalar(u8, ws_chars, c)) |_| {
+        return true;
+    }
 
     return false;
 }
 
 test "lex hash" {
-    //const data = "  # foo\n\nbar\n> Quote!\n```foo```";
     const data =
         \\# Header!
+        \\## Header 2
         \\  some generic text here
         \\
         \\after the break...
@@ -189,6 +244,9 @@ test "lex hash" {
         \\code
         \\```
     ;
+
+    // var gpa = GPA(.{}){};
+    // var alloc = gpa.allocator();
 
     var lex: Lexer = Lexer.init(data);
     var token = lex.next();
