c1266d516bc876c4fed136d72fce93f258e7f34d
JacobCrabill <jacob.crabill@gmail.com>
2024-09-04 19:35:23 -0700

    nvim: Lazy-load the plugin to speedup startup

--- plugin/zigdown.lua
+++ plugin/zigdown.lua
@@ -1,5 +1,3 @@
-local zigdown = require('zigdown')
-
 -- Expose relevant functions as user commands
-vim.api.nvim_create_user_command('Zigdown', zigdown.render_current_buffer, {})
-vim.api.nvim_create_user_command('ZigdownRebuild', zigdown.install, {})
+vim.api.nvim_create_user_command('Zigdown', function() require('zigdown').render_current_buffer() end, {})
+vim.api.nvim_create_user_command('ZigdownRebuild', function() require('zigdown').install() end, {})
