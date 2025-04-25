7e20fbbd6d29c44d891be9e908e7c939b65eb513
Jacob Crabill <jacob.crabill@gmail.com>
2025-04-25 14:10:05 -0700

    Bug fix list parsing

--- src/parsers/utils.zig
+++ src/parsers/utils.zig
@@ -202,7 +202,8 @@ pub fn isUnorderedListItem(line: []const Token) bool {
                 if (have_bullet) return true;
                 return false;
             },
-            .PLUS, .MINUS, .STAR => {
+            .PLUS, .MINUS => {
+                // Note that a '*' can refer to formatting, so ignore it here
                 if (have_bullet) return false; // Can only have one bullet character
                 have_bullet = true;
             },
