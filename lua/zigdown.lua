32771a720d9a91833c9d14e3387cdb983da9c9c7
Jacob Crabill <jacob.crabill@gmail.com>
2025-08-25 16:56:15 -0700

    lua: Fix load time
    Previously, the 'setup()' function would _also_ check if Zigdown was
    already built, and if not, build it. Doing all of the setup work
    necessary to start the background build job added a noticeable delay to
    Neovim's startup.
    
    Now, the 'setup()' function only assigns config options; building and
    loading of the plugin is deferred to the first render or format call.

--- lua/zigdown.lua
+++ lua/zigdown.lua
@@ -1,18 +1,12 @@
-local utils = require "zigdown.utils"
-local build = require "zigdown.build"
-local render = require "zigdown.render"
-
--- For luajit 2.1.0 (Based on Lua 5.1)
--- Import the shared library containing our Lua API to Zigdown
-local plugin_root = utils.get_zigdown_root()
-vim.opt.runtimepath:append(plugin_root)
-
 -- Our Plugin Module Table
 local M = {}
+
+M.root = nil
+
+-- Setup any default values on options, if any
 M.opts = {
   format_width = 100,
 }
-M.root = plugin_root
 
 -- Required version of the Zig compiler
 local zig_ver = "0.15.1"
@@ -20,14 +14,25 @@ local zig_ver = "0.15.1"
 --- Setup the plugin with user-provided options
 function M.setup(opts)
   M.opts = vim.tbl_deep_extend("force", M.opts, opts or {})
+end
 
-  -- Check if the plugin has been built yet. Build it if not.
+-- Check if the plugin has been built yet. Build it if not.
+function M.ensure_loaded()
+  local utils = require("zigdown.utils")
+  local build = require("zigdown.build")
+
+  if M.root == nil then
+    -- For luajit 2.1.0 (Based on Lua 5.1)
+    -- Import the shared library containing our Lua API to Zigdown
+    local plugin_root = utils.get_zigdown_root()
+    M.root = plugin_root
+    vim.opt.runtimepath:append(plugin_root)
+  end
+
   local lib_file = build.get_plugin_name()
   if not utils.file_exists(lib_file) then
     M.install()
   end
-
-  -- Setup any default values on options, if any
 end
 
 -- Render the current Markdown buffer to a new Terminal window
@@ -36,6 +41,8 @@ function M.render_current_buffer()
     vim.notify("Can only render Markdown content!", vim.log.levels.WARN)
     return
   end
+
+  M.ensure_loaded()
 
   -- Create an autocmd group to automatically re-render the buffer upon save
   -- (Effectively a live preview pane)
@@ -47,7 +54,7 @@ function M.render_current_buffer()
   })
 
   -- Render the Markdown in-process using the Lua module and nvim APIs.
-  render.render_buffer_lua(0)
+  require("zigdown.render").render_buffer_lua(0)
 end
 
 function M.format_current_buffer()
@@ -55,17 +62,20 @@ function M.format_current_buffer()
     vim.notify("Can only render Markdown content!", vim.log.levels.WARN)
     return
   end
-  render.format_buffer(0, M.opts.format_width)
+
+  M.ensure_loaded()
+
+  require("zigdown.render").format_buffer(0, M.opts.format_width)
 end
 
 -- Clear the Vim autocommand group to cancel the render-on-save
 function M.cancel_auto_render()
-  render.clear_autogroup()
+  require("zigdown.render").clear_autogroup()
 end
 
--- Rebuild the zig code
+-- Rebuild the Zigdown Lua module
 function M.install()
-  build.install(zig_ver, M.root)
+  require("zigdown.build").install(zig_ver, M.root)
 end
 
 return M
